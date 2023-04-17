cdb132eeb323b3b2a1db20a6330e48cc85f4807f
Reynardo Perez <reynardo19@hotmail.com>
2023-04-16 23:44:23 -0400

    adds AStart pathfinding library (@valantonini)

---
+++ DungeonInspector/Assets/Editor/SandBox/Game/PathFindingExample.cs
@@ -0,0 +1,41 @@
+﻿using AStar.Options;
+using AStar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Assets.Editor.SandBox.Game
+{
+    internal class PathFindingExample
+    {
+        public void Pathfinding()
+        {
+            var pathfinderOptions = new PathFinderOptions
+            {
+                PunishChangeDirection = true,
+                UseDiagonals = false,
+            };
+
+            var tiles = new short[,]
+            {
+                { 1, 0, 1 },
+                { 1, 0, 1 },
+                { 1, 1, 1 },
+            };
+
+            var worldGrid = new WorldGrid(tiles);
+            var pathfinder = new PathFinder(worldGrid, pathfinderOptions);
+
+            // The following are equivalent:
+
+            // matrix indexing
+            Position[] path = pathfinder.FindPath(new Position(0, 0), new Position(0, 2));
+
+            // point indexing
+            //Point[] path = pathfinder.FindPath(new Point(0, 0), new Point(2, 0));
+        }
+    }
+}
