76e968c47f0d3008f8a61d91c8a234c45a759663
Reynardo Perez <reynardo19@hotmail.com>
2023-04-24 03:41:24 -0400

    DEngine refactor (wip)

--- DEngine/DEngine/Main/DEngine.cs
+++ DEngine/DEngine/Main/DEngine.cs
@@ -9,48 +9,32 @@ namespace DungeonInspector
 {
     public class DEngine
     {
-       // private readonly EngineSystemBase _components;
         private readonly DEditorSystem _editorSystem;
-        //private readonly EngineSystemBase _renderer;
-        //private readonly EngineSystemBase _physics;
-        //private readonly EngineSystemBase _input;
-        //private readonly EngineSystemBase _audio;
-        //private readonly EngineSystemBase _time;
+   
 
         private readonly DSandboxBase _playModeSandBox;
         private readonly DSandboxBase _editModeSandBox;
 
         private DSandboxBase _currentSandBox;
-        private List<EngineSystemBase> _currentServices;
+        private EngineSystemBase[] _currentServices;
 
         public DEngine(DSandboxBase playModeSandBox, DSandboxBase editModeSandBox)
         {
             _playModeSandBox = playModeSandBox;
             _editModeSandBox = editModeSandBox;
 
-            new DIEngineCoreServices();
-            new Utils();
 
-            _currentServices = new List<EngineSystemBase>();
+            _currentServices = ConstructServices(editModeSandBox);
 
-            ConstructServices(_currentServices, editModeSandBox);
-
-            Debug.Log("ok1");
-
-            //_time = DIEngineCoreServices.Get<DTime>();
-            //_input = DIEngineCoreServices.Get<DInput>();
-            //_audio = DIEngineCoreServices.Get<DAudioSystem>();
             _editorSystem = DIEngineCoreServices.Get<DEditorSystem>();
-            //_physics = DIEngineCoreServices.Get<DPhysicsController>();
-            //_renderer = DIEngineCoreServices.Get<DRenderingController>();
-            //_components = DIEngineCoreServices.Get<DEntitiesController>();
+          
 
             _editorSystem.Init();
 
             _editorSystem.Toolbar.OnPlayBegin += OnPlayBegin; 
             _editorSystem.Toolbar.OnPlayEnd += OnPlayEnd;
 
-            OnPlayEnd();
+            RunSandbox(_editModeSandBox);
         }
 
 
@@ -68,9 +52,11 @@ namespace DungeonInspector
         {
             _currentSandBox?.OnQuit();
             _currentSandBox = GetSandboxCopy(target);
+            _currentServices = ConstructServices(target);
+
             _currentSandBox.OnInitialize();
 
-            for (int i = 0; i < _currentServices.Count; i++)
+            for (int i = 0; i < _currentServices.Length; i++)
             {
                 _currentServices[i].Init();
             }
@@ -78,56 +64,42 @@ namespace DungeonInspector
 
         public void Update()
         {
-            for (int i = 0; i < _currentServices.Count; i++)
+            for (int i = 0; i < _currentServices.Length; i++)
             {
                 _currentServices[i].Update();
             }
 
             _editorSystem.Update();
-            //_time.Update();
-            //_input.Update();
-            //_components.Update();
-            //_physics.Update();
-            //_renderer.Update();
-            //_editorSystem.Update();
         }
 
         private DSandboxBase GetSandboxCopy(DSandboxBase original)
         {
-            var copy = (DSandboxBase)Activator.CreateInstance(original.GetType(), (object)original.Services);
-
-            ConstructServices(_currentServices, original);
-
-            return copy;
+            return (DSandboxBase)Activator.CreateInstance(original.GetType(), (object)original.Services);
         }
 
-        private void ConstructServices(List<EngineSystemBase> services, DSandboxBase sandbox)
+        private EngineSystemBase[] ConstructServices(DSandboxBase sandbox)
         {
-            services.Clear();
+            var services = new EngineSystemBase[sandbox.Services.Length];
+
             new DIEngineCoreServices();
+            new Utils();
 
             for (int i = 0; i < sandbox.Services.Length; i++)
             {
-                services.Add(DIEngineCoreServices.Get(sandbox.Services[i]));
+                services[i] = DIEngineCoreServices.Get(sandbox.Services[i]);
             }
+
+            return services;
         }
 
         public void Destroy()
         {
             _currentSandBox?.OnQuit();
 
-            for (int i = 0; i < _currentServices.Count; i++)
+            for (int i = 0; i < _currentServices.Length; i++)
             {
                 _currentServices[i].Cleanup();
             }
-
-            //_time.Cleanup();
-            //_audio.Cleanup();
-            //_input.Cleanup();
-            //_components.Cleanup();
-            //_physics.Cleanup();
-            //_renderer.Cleanup();
-            //_editorSystem.Cleanup();
         }
     }
 }
--- DungeonInspector/Assets/Editor/SandBox/DTilemapRenderer.cs
+++ DungeonInspector/Assets/Editor/SandBox/DTilemapRenderer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace DungeonInspector
 {
